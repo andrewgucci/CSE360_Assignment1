4c9ba4eddd5b457eaf55ca9344bf10254e439854
Andrew Thomas <athoma67@asu.edu>
2020-10-06 22:58:33 -0700

    Initial commit of Assignment 1 onto GitHub.

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,69 @@
+/**
+ * @author Andrew Thomas
+ * @version 1.0
+ * CSE 360: Assignment 1
+ * Javier Gonzalez-Sanchez
+ * 10/6/20
+ */
+
+package cse360assignment02;
+
+/**
+ * public class AddingMachine which will allow integers to be added and subtracted together
+ * as well as total their and print their resulting values.
+ */
+public class AddingMachine 
+{
+	
+	private int total;
+  
+	/**
+	 * constructor to be implemented to create an AddingMachine object.
+	 */
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+  
+	/**
+	 * getTotal method to be implemented to return a total of integers added and subtracted.
+	 * @return the total of all integers added and subtracted.
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+  
+	/**
+	 * add method to be implemented to add an integer value passed in.
+	 * @param value is the integer value to be added.
+	 */
+	public void add (int value) 
+	{
+	}
+
+	/**
+	 * subtract method to be implemented to subtract an integer value passed in.
+	 * @param value is the integer value to be subtracted.
+	 */
+	public void subtract (int value) 
+	{
+	}
+
+	/**
+	 * toString method to be implemented to return a particular string.
+	 * @return a particular string.
+	 */
+	public String toString () 
+	{
+		return "";
+  	}
+
+	/**
+	 * clear method to be implemented to clear some data values.
+	 */
+	public void clear() 
+	{
+  	}
+	
+}
