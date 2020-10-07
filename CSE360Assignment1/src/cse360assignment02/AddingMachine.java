9c72e5cd923418e35e62293846ccd3f8889fdaea
andrewgucci <andrewgucci@192.168.1.5>
2020-10-07 01:32:54 -0700

    Implemented the AddingMachine class constructor as well as the methods getTotal, add, subtract, toString, and clear.

--- CSE360Assignment1/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -1,69 +1,93 @@
+
+
 /**
  * @author Andrew Thomas
- * @version 1.0
+ * @version 2.0
  * CSE 360: Assignment 1
  * Javier Gonzalez-Sanchez
- * 10/6/20
+ * 10/7/20
  */
 
 package cse360assignment02;
 
+
 /**
  * public class AddingMachine which will allow integers to be added and subtracted together
- * as well as total their and print their resulting values.
+ * as well as total and print their resulting values.
  */
 public class AddingMachine 
 {
 	
 	private int total;
-  
+	private String operationsPerformed;
+	
 	/**
-	 * constructor to be implemented to create an AddingMachine object.
+	 * constructor which creates an AddingMachine object that has methods to print and manipulate a total of integers.
 	 */
 	public AddingMachine () 
 	{
-		total = 0;  // not needed - included for clarity
+		
+		total = 0;
+		operationsPerformed = "0";
+		
 	}
   
 	/**
-	 * getTotal method to be implemented to return a total of integers added and subtracted.
-	 * @return the total of all integers added and subtracted.
+	 * getTotal method which returns the total integer value of all integers added or subtracted.
+	 * @return the total of all integers added and or subtracted.
 	 */
 	public int getTotal () 
 	{
-		return 0;
+		
+		return total;
+		
 	}
   
 	/**
-	 * add method to be implemented to add an integer value passed in.
-	 * @param value is the integer value to be added.
+	 * add method which adds the integer parameter value to total and update operationsPerformed with the addition performed.
+	 * @param value is the integer to be added to total and append to operationsPerformed.
 	 */
 	public void add (int value) 
 	{
+		
+		total = total + value;
+		operationsPerformed = operationsPerformed + " + " + value;
+		
 	}
 
 	/**
-	 * subtract method to be implemented to subtract an integer value passed in.
-	 * @param value is the integer value to be subtracted.
+	 * subtract method which subtracts the integer parameter value from total and update operationsPerformed with the subtraction performed.
+	 * @param value is the integer to be subtracted from total and appended to operationsPerformed.
 	 */
 	public void subtract (int value) 
 	{
+		
+		total = total - value;
+		operationsPerformed = operationsPerformed + " - " + value;
+		
 	}
 
 	/**
-	 * toString method to be implemented to return a particular string.
-	 * @return a particular string.
+	 * toString method which returns operationsPerformed containing all the operations performed on the total.
+	 * @return operationsPerformed the string containing all subtraction and addition operations performed.
 	 */
 	public String toString () 
 	{
-		return "";
+		
+		return operationsPerformed;
+		
   	}
 
 	/**
-	 * clear method to be implemented to clear some data values.
+	 * clear method which resets or, clears, the values of both total and operationsPerformed to 0.
 	 */
 	public void clear() 
 	{
+		
+		total = 0;
+		operationsPerformed = "0";
+		
   	}
 	
 }
+
